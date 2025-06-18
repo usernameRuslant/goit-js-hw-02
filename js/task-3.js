7cb3cbfca9edda179bab2110082acbe564238a80
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-06-18 15:28:59 +0300

    js tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,34 @@
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-}
+//==========================================================================================================================
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+// Доповни код функції таким чином, що:
+
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+//==========================================================================================================================
+function checkForSpam(message) {
+  // const cM = message.toLowerCase();
+  // console.log(cM);
+  // if (cM.includes('spam')) {
+  //   return true;
+  // } else if (cM.includes('sale')) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  //2
+  const cM = message.toLowerCase();
+  return cM.includes('spam') || cM.includes('sale');
+  //3
+  // return (
+  //   message.toLowerCase().includes('spam') ||
+  //   message.toLowerCase().includes('sale')
+  // );
+}
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[ SPAM] How to earn fast money?'));
